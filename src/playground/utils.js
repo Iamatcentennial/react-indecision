b3e3d8f23cebdfee677d6c141573a15079246679
Ashish <ashish.raist@gmail.com>
2018-08-28 21:37:27 -0400

    Remove duplicated directory

--- src/playground/utils.js
+++
@@ -1,8 +0,0 @@
-export default (age)=>{
-    if(age<18){
-        console.log('You can not drink');
-    }else{
-        console.log('You can drink');
-    }
-}
-//export { ageValidaation as default}
